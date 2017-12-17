0b909788d97cbfc2c999de94e01233ddedff8382
Oskar Polak <oskarpolak@vp.pl>
2017-12-17 17:54:03 +0100

    as

--- src/main/java/pl/oskarpolak/demorepo/controllers/LoginController.java
+++ src/main/java/pl/oskarpolak/demorepo/controllers/LoginController.java
@@ -2,14 +2,29 @@ package pl.oskarpolak.demorepo.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
     public String login(){
-        return "login";
+        return "testForm";
+    }
+
+    @PostMapping("/login")
+    public String loginPost(@RequestParam("login") String login,
+                            @RequestParam("password") String password,
+                            Model model){
+        if(login.equals("admin") && password.equals("admin")){
+            model.addAttribute("error", "Zalogowano poprawnie!");
+        }else{
+            model.addAttribute("error", "Dane sa niepoprawne");
+        }
+        return "testForm";
     }
 
 }
