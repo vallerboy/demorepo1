9bf8b991ebcf85138ce44ab0a9c53b6ed91b5e48
Oskar Polak <oskarpolak@vp.pl>
2017-12-17 17:02:36 +0100

    asd

--- src/main/java/pl/oskarpolak/demorepo/controllers/MainController.java
+++ src/main/java/pl/oskarpolak/demorepo/controllers/MainController.java
@@ -2,10 +2,7 @@ package pl.oskarpolak.demorepo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -17,5 +14,14 @@ public class MainController {
                         Model model){
        model.addAttribute("age", someAge);
        return "index";
+   }
+
+   @PostMapping("/")
+    public String indexPost(@RequestParam(name = "name", required = false) String name,
+                            @RequestParam("message") String message,
+                            @RequestParam("email") String email){
+
+       System.out.println(name + " " + message + " " + email);
+        return "redirect:/0";
    }
 }
